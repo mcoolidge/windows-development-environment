fdefca76cda55084f51c66d1670fb7b9f3c67dfc
mcoolidge <mat735@gmail.com>
2018-11-18 09:12:35 -0500

    Added notepad++ and rapidee, commented out stuff I dont need

--- env-windows.ps1
+++ env-windows.ps1
@@ -42,12 +42,6 @@ git config --global alias.everything "! git pull && git submodule update --init 
 git config --global alias.aliases "config --get-regexp alias"
 
 #
-# AWS awscli
-#
-choco install awscli --yes
-Update-Environment-Path
-
-#
 # MinGW
 # 
 
@@ -69,9 +63,9 @@ Update-Environment-Path
 #
 # Languages
 #
-choco install php --yes
-choco install ruby --yes
-choco install ruby2.devkit --yes
+# choco install php --yes
+# choco install ruby --yes
+# choco install ruby2.devkit --yes
 choco install python2 --yes
 choco install jdk8 --yes
 Update-Environment-Path
@@ -82,10 +76,10 @@ Update-Environment-Path
 npm install --global --production npm-windows-upgrade
 npm-windows-upgrade --npm-version latest
 npm install -g gulp-cli 
-npm install -g yo
-npm install -g mocha
-npm install -g install-peerdeps
-npm install -g typescript
+# npm install -g yo
+# npm install -g mocha
+# npm install -g install-peerdeps
+# npm install -g typescript
 # npm install prettier-eslint --save-dev
 
 #
@@ -102,9 +96,9 @@ choco install docker-for-windows --yes
 
 Update-Environment-Path
 
-docker pull worpress
-docker pull mysql
-docker pull phpmyadmin
+# docker pull worpress
+# docker pull mysql
+# docker pull phpmyadmin
 
 
 # Yarn
@@ -118,10 +112,10 @@ npm install -g grunt-cli
 
 # ESLint
 npm install -g eslint
-npm install -g babel-eslint
-npm install -g eslint-plugin-react
-npm install -g install-peerdeps
-install-peerdeps --dev eslint-config-airbnb
+# npm install -g babel-eslint
+# npm install -g eslint-plugin-react
+# npm install -g install-peerdeps
+# install-peerdeps --dev eslint-config-airbnb
 
 #
 # VS Code
@@ -139,20 +133,20 @@ code --install-extension ms-vscode.PowerShell
 code --install-extension ms-vscode.csharp
 
 # PHP support
-code --install-extension felixfbecker.php-debug
-code --install-extension HvyIndustries.crane
+# code --install-extension felixfbecker.php-debug
+# code --install-extension HvyIndustries.crane
 
 # Ruby support
-code --install-extension rebornix.Ruby
+# code --install-extension rebornix.Ruby
 
 # HTML, CSS, JavaScript support
 code --install-extension Zignd.html-css-class-completion
 code --install-extension lonefy.vscode-JS-CSS-HTML-formatter
-code --install-extension robinbentley.sass-indented
+# code --install-extension robinbentley.sass-indented
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension RobinMalfait.prettier-eslint-vscode
 code --install-extension flowtype.flow-for-vscode
-code --install-extension dzannotti.vscode-babel-coloring
+# code --install-extension dzannotti.vscode-babel-coloring
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-rename-tag
 
@@ -161,13 +155,13 @@ code --install-extension eg2.vscode-npm-script
 code --install-extension christian-kohler.npm-intellisense
 
 # Mocha support
-code --install-extension spoonscen.es6-mocha-snippets
-code --install-extension maty.vscode-mocha-sidebar
+# code --install-extension spoonscen.es6-mocha-snippets
+# code --install-extension maty.vscode-mocha-sidebar
 
 # React Native support
-code --install-extension vsmobile.vscode-react-native
-npm install -g create-react-native-app
-npm install -g react-native-cli
+# code --install-extension vsmobile.vscode-react-native
+# npm install -g create-react-native-app
+# npm install -g react-native-cli
 
 # Docker support
 code --install-extension PeterJausovec.vscode-docker
@@ -179,8 +173,8 @@ code --install-extension jebbs.plantuml
 # MySQL
 #
 
-choco install mysql --yes
-choco install mysql.workbench --yes
+# choco install mysql --yes
+# choco install mysql.workbench --yes
 
 
 #
@@ -194,14 +188,15 @@ choco install androidstudio --yes
 #
 
 # Hugo
-choco install hugo --yes
+# choco install hugo --yes
 
 #
 # Basic Utilities
 #
 
-# choco install slack --yes
-# choco install xenulinksleuth --yes
+choco install slack --yes
+choco install notepadplusplus --yes
+choco install rapidee --yes
 
 # File Management
 choco install beyondcompare --yes
