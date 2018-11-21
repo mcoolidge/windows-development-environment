abfd504b6526276928750d314cc93fa675b5040e
Mat Coolidge <matcoolidge@gmail.com>
2018-11-21 14:46:04 -0500

    removed unused packages

--- env-windows.ps1
+++ env-windows.ps1
@@ -31,7 +31,7 @@ Update-Environment-Path
 #
 
 choco install git --yes
-choco install tortoisegit --yes
+# choco install tortoisegit --yes
 Update-Environment-Path
 git config --global alias.pom 'pull origin master'
 git config --global alias.last 'log -1 HEAD'
@@ -40,18 +40,6 @@ git config --global alias.ammend "commit -a --amend"
 git config --global alias.standup "log --since yesterday --author $(git config user.email) --pretty=short"
 git config --global alias.everything "! git pull && git submodule update --init --recursive"
 git config --global alias.aliases "config --get-regexp alias"
-
-#
-# MinGW
-# 
-
-choco install mingw --yes
-Update-Environment-Path
-
-# Get-Command mingw32-make
-
-# todo: Alias `make` to `mingw32-make` in Git Bash
-# todo: Write `mingw32-make %*` to make.bat in MinGW install directory
 
 #
 # Caddy HTTP Server
@@ -63,9 +51,6 @@ Update-Environment-Path
 #
 # Languages
 #
-# choco install php --yes
-# choco install ruby --yes
-# choco install ruby2.devkit --yes
 choco install python2 --yes
 choco install jdk8 --yes
 Update-Environment-Path
@@ -94,10 +79,6 @@ Update-Environment-Path
 # WSL
 choco install -y wsl
 
-
-# Bower
-npm install -g bower
-
 # Grunt
 npm install -g grunt-cli
 
@@ -122,11 +103,9 @@ code --install-extension ms-vscode.csharp
 # HTML, CSS, JavaScript support
 code --install-extension Zignd.html-css-class-completion
 code --install-extension lonefy.vscode-JS-CSS-HTML-formatter
-# code --install-extension robinbentley.sass-indented
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension RobinMalfait.prettier-eslint-vscode
 code --install-extension flowtype.flow-for-vscode
-# code --install-extension dzannotti.vscode-babel-coloring
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-rename-tag
 
@@ -137,24 +116,19 @@ code --install-extension christian-kohler.npm-intellisense
 # Docker support
 code --install-extension PeterJausovec.vscode-docker
 
-# PlantUML support
-code --install-extension jebbs.plantuml
-
 #
 # Android Studio
 # 
-
 choco install androidstudio --yes
 
 #
 # Basic Utilities
 #
-
 choco install slack --yes
 choco install notepadplusplus --yes
 choco install rapidee --yes
-choco install -y keepass
-choco install -y paint.net
+choco install keepass  --yes
+choco install paint.net  --yes
 
 
 # File Management
@@ -177,7 +151,6 @@ choco install firefox --yes
 # Misc
 choco install sysinternals --yes
 choco install procexp --yes
-choco install awscli --yes
 choco install firacode --yes # See https://www.youtube.com/watch?v=KI6m_B1f8jc
 
 Update-Environment-Path
